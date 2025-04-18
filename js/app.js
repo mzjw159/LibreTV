1caf09392bfa5d44938033d6645b627f6df5b9f6
mzjw159 <63756539+mzjw159@users.noreply.github.com>
2025-04-18 19:55:29 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -737,16 +737,9 @@ async function search() {
                                 </p>
                             </div>
                             
-                            <div class="flex justify-between items-center mt-2 pt-2 border-t border-gray-800">
+                            <div class="flex justify-between items-center mt-2 pt-2 border-t border-gray-800">来源：
                                 ${sourceInfo ? `<div>${sourceInfo}</div>` : '<div></div>'}
                                 <div>
-                                    <span class="text-xs text-gray-500 flex items-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                        </svg>
-                                        点击播放
-                                    </span>
                                 </div>
                             </div>
                         </div>
